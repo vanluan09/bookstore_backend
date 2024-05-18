47fda78ade4d448b4798666a0bb81fd95586c770
vanluan09 <gioanluan09@gmail.com>
2024-05-18 09:28:35 +0700

    first commit

--- src/services/JwtService.js
+++ src/services/JwtService.js
@@ -5,7 +5,7 @@ dotenv.config()
 const generalAccessToken = async (payload) => {
     const access_token = jwt.sign({
         ...payload
-    }, process.env.ACCESS_TOKEN, { expiresIn: '30s' })
+    }, process.env.ACCESS_TOKEN, { expiresIn: '1h' })
 
     return access_token
 }
